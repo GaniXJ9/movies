245e7f667bc87ec239550a61477024d1df539436
Gani Galiyev <ganiv847@gmail.com>
2026-02-11 12:14:53 +0500

    fix: dayjs instalation path

--- Movies/src/main.tsx
+++ Movies/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import Header from "./widgets/header.tsx";
 import "./shared/css/style.css";
 
 createRoot(document.getElementById("root")!).render(
